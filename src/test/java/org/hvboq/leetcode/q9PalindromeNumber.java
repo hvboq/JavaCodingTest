b9b1312b06162cb8900794b84386654c3d993693
hvboq <hvboq11@gmail.com>
2024-05-05 03:22:31 +0900

    [MODIFY] readability increase

--- src/test/java/org/hvboq/leetcode/q9PalindromeNumber.java
+++ src/test/java/org/hvboq/leetcode/q9PalindromeNumber.java
@@ -4,32 +4,30 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class q9PalindromeNumber {
-    //    public boolean isPalindrome(int x) {
-//        char[] charArr = (Integer.toString(x)).toCharArray();
-//        for (int i = 0; i <= charArr.length / 2; i++) {
-//            if (charArr[i] != charArr[charArr.length - (i + 1)]) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-    //최초 풀이
     public boolean isPalindrome(int x) {
-        if (x == 0)
-            return true;
-        else if (x < 0 || x % 10 == 0) {
-            return false;
+        char[] charArr = (Integer.toString(x)).toCharArray();
+        for (int i = 0; i <= charArr.length / 2; i++) {
+            if (charArr[i] != charArr[charArr.length - (i + 1)]) {
+                return false;
+            }
         }
-        int reverseNum = x % 10;
-        while (reverseNum < x) {
-            x = x / 10;
-            reverseNum = reverseNum * 10 + x % 10;
-        }
-        if (x == reverseNum)
-            return true;
-        else
-            return false;
+        return true;
     }
+
+//    public boolean isPalindrome(int x) {
+//        if (x == 0)
+//            return true;
+//        else if (x < 0 || x % 10 == 0) {
+//            return false;
+//        }
+//        int reverseNum = 0;
+//        while (reverseNum < x) {
+//            int digit = x % 10;
+//            reverseNum = reverseNum * 10 + digit;
+//            x = x / 10;
+//        }
+//        return x == reverseNum || x == reverseNum / 10;
+//    }
 
 
     @Test
