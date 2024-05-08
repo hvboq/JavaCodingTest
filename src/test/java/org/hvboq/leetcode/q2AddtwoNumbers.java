8f052d2e45ff5ffa436e61f058768a69aec4d5a8
hvboq <hvboq11@gmail.com>
2024-05-08 14:52:12 +0900

    [MODIFY] leetcode q2 add different

--- src/test/java/org/hvboq/leetcode/q2AddtwoNumbers.java
+++ src/test/java/org/hvboq/leetcode/q2AddtwoNumbers.java
@@ -6,22 +6,57 @@ import org.junit.jupiter.api.Test;
 import java.util.Stack;
 
 public class q2AddtwoNumbers {
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode l1CurrentNode = l1;
+//        ListNode l2CurrentNode = l2;
+//
+//        boolean olim = false;
+//        Stack<Integer> answerStack = new Stack<>();
+//        while (l1CurrentNode != null || l2CurrentNode != null || olim) {
+//            int currentInteger = olim ? 1 : 0;
+//            if (l1CurrentNode != null) {
+//                currentInteger += l1CurrentNode.val;
+//                l1CurrentNode = l1CurrentNode.next;
+//            }
+//            if (l2CurrentNode != null) {
+//                currentInteger += l2CurrentNode.val;
+//                l2CurrentNode = l2CurrentNode.next;
+//            }
+//            if (currentInteger > 9) {
+//                olim = true;
+//                currentInteger -= 10;
+//            } else {
+//                olim = false;
+//            }
+//            System.out.println(currentInteger);
+//            answerStack.push(currentInteger);
+//        }
+//
+//        ListNode answerNode = null;
+//        while (!answerStack.empty()) {
+//            ListNode currentNode = new ListNode(answerStack.pop(), answerNode);
+//            answerNode = currentNode;
+//        }
+//
+//        return answerNode;
+//    }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode l1CurrentNode = l1;
-        ListNode l2CurrentNode = l2;
+        ListNode headNode = new ListNode();
+        ListNode currentNode = headNode;
+        headNode.next = currentNode;
 
         boolean olim = false;
         Stack<Integer> answerStack = new Stack<>();
-        while (l1CurrentNode != null || l2CurrentNode != null || olim) {
+        while (l1 != null || l2 != null || olim) {
             int currentInteger = olim ? 1 : 0;
-            if (l1CurrentNode != null) {
-                currentInteger += l1CurrentNode.val;
-                l1CurrentNode = l1CurrentNode.next;
+            if (l1 != null) {
+                currentInteger += l1.val;
+                l1 = l1.next;
             }
-            if (l2CurrentNode != null) {
-                currentInteger += l2CurrentNode.val;
-                l2CurrentNode = l2CurrentNode.next;
+            if (l2 != null) {
+                currentInteger += l2.val;
+                l2 = l2.next;
             }
             if (currentInteger > 9) {
                 olim = true;
@@ -29,17 +64,12 @@ public class q2AddtwoNumbers {
             } else {
                 olim = false;
             }
-            System.out.println(currentInteger);
-            answerStack.push(currentInteger);
+
+            currentNode.next = new ListNode(currentInteger);
+            currentNode = currentNode.next;
         }
 
-        ListNode answerNode = null;
-        while (!answerStack.empty()) {
-            ListNode currentNode = new ListNode(answerStack.pop(), answerNode);
-            answerNode = currentNode;
-        }
-
-        return answerNode;
+        return headNode.next;
     }
 
     @Test
