6891d85f5820837d03b5ce8e12ae2ab4eff6746a
hvboq <hvboq11@gmail.com>
2024-05-09 02:30:47 +0900

    [MODIFY] baekjoon q1914

--- src/main/java/org/hvboq/programers/Main.java
+++ src/main/java/org/hvboq/programers/Main.java
@@ -1,134 +1,28 @@
 package org.hvboq.programers;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
+    static int count = 0;
+
     public static void main(String[] args) {
-        class Tomato {
-            int x, y, day;
-
-            public Tomato(int x, int y, int day) {
-                this.x = x;
-                this.y = y;
-                this.day = day;
-            }
-        }
-        Queue<Tomato> ripeTomatos = new LinkedList<>();
-        int[] xValues = {-1, 0, 1, 0};
-        int[] yValues = {0, 1, 0, -1};
-
-        int day = 0;
-
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt(), n = sc.nextInt();
+        int n = sc.nextInt();
 
-        int[][] tomatos = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int tomato = sc.nextInt();
-                tomatos[i][j] = tomato;
-                if (tomato == 1) {
-                    ripeTomatos.add(new Tomato(j, i, 0));
-                }
-            }
-        }
-
-        while (!ripeTomatos.isEmpty()) {
-            Tomato tomato = ripeTomatos.poll();
-            for (int i = 0; i < 4; i++) {
-                if (tomato.x + xValues[i] >= 0 && tomato.x + xValues[i] < m
-                        && tomato.y + yValues[i] >= 0 && tomato.y + yValues[i] < n) {
-                    if (tomatos[tomato.y + yValues[i]][tomato.x + xValues[i]] == 0) {
-                        tomatos[tomato.y + yValues[i]][tomato.x + xValues[i]] = 1;
-                        day = tomato.day + 1;
-                        ripeTomatos.add(
-                                new Tomato(tomato.x + xValues[i], tomato.y + yValues[i], day));
-
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (tomatos[i][j] == 0) {
-                    System.out.println(-1);
-                    return;
-                }
-            }
-        }
-        System.out.println(day);
+        StringBuilder sb = new StringBuilder();
+        q1914HanoiTower(n, '1', '2', '3', sb);
+        System.out.println(count);
+        System.out.println(sb);
     }
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int m = sc.nextInt(), n = sc.nextInt();
-//
-//        int[][] tomatos = new int[n][m];
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < m; j++) {
-//                tomatos[i][j] = sc.nextInt();
-//            }
-//        }
-//        for (int i = 0; i < m * n; i++) {
-//            boolean isGood = true;
-//            for (int j = 0; j < n; j++) {
-//                for (int k = 0; k < m; k++) {
-//                    if (tomatos[j][k] == 0) {
-//                        isGood = false;
-//                        break;
-//                    }
-//                }
-//            }
-//            int[][] goodedTomatos = new int[n][m];
-//            if (isGood) {
-//                System.out.println(i);
-//                return;
-//            } else {
-//                for (int j = 0; j < n; j++) {
-//                    for (int k = 0; k < m; k++) {
-//                        if (goodedTomatos[j][k] == 1)
-//                            continue;
-//                        goodedTomatos[j][k] = tomatos[j][k];
-//                        if (tomatos[j][k] == 1) {
-//                            if (j > 0) {
-//                                if (tomatos[j - 1][k] == 0)
-//                                    goodedTomatos[j - 1][k] = 1;
-//                            }
-//                            if (k > 0) {
-//                                if (tomatos[j][k - 1] == 0)
-//                                    goodedTomatos[j][k - 1] = 1;
-//                            }
-//
-//                            if (j < n - 1) {
-//                                if (tomatos[j + 1][k] == 0)
-//                                    goodedTomatos[j + 1][k] = 1;
-//                            }
-//                            if (k < m - 1) {
-//                                if (tomatos[j][k + 1] == 0)
-//                                    goodedTomatos[j][k + 1] = 1;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//
-//            boolean isFailed = true;
-//            for (int j = 0; j < n; j++) {
-//                for (int k = 0; k < m; k++) {
-//                    if (tomatos[j][k] != goodedTomatos[j][k]) {
-//                        isFailed = false;
-//                    }
-//                }
-//            }
-//            if (isFailed) {
-//                System.out.println(-1);
-//                return;
-//            } else {
-//                tomatos = goodedTomatos;
-//            }
-//        }
-//    }
+
+    public static void q1914HanoiTower(int n, char from, char mid, char to, StringBuilder sb) {
+        count++;
+        if (n == 1) {
+            sb.append(from + " " + to + "\n");
+            return;
+        }
+        q1914HanoiTower(n - 1, from, to, mid, sb);
+        sb.append(from + " " + to + "\n");
+        q1914HanoiTower(n - 1, mid, from, to, sb);
+    }
 }
--- src/test/java/org/hvboq/baekjoon/q1914HanoiTower.java
+++ src/test/java/org/hvboq/baekjoon/q1914HanoiTower.java
@@ -4,19 +4,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class q1914HanoiTower {
-    public void q1914HanoiTower(int n, char from, char mid, char to) {
+    static int count = 0;
+
+    public void q1914HanoiTower(int n, char from, char mid, char to, StringBuilder sb) {
+        count++;
         if (n == 1) {
-            System.out.println("원반 1을 " + from + " 에서 " + to + "로 이동");
+            sb.append(from + " " + to + "\n");
             return;
         }
-        q1914HanoiTower(n - 1, from, to, mid);
-        System.out.println("원반 " + n + "을 " + from + " 에서 " + to + "로 이동");
-        q1914HanoiTower(n - 1, mid, from, to);
+        q1914HanoiTower(n - 1, from, to, mid, sb);
+        sb.append(from + " " + to + "\n");
+        q1914HanoiTower(n - 1, mid, from, to, sb);
     }
 
     @Test
     @DisplayName("q1914HanoiTower")
     void test() {
-        q1914HanoiTower(5, 'A', 'B', 'C');
+        StringBuilder sb = new StringBuilder();
+        q1914HanoiTower(3, '1', '2', '3', sb);
+        System.out.println(count);
+        System.out.println(sb);
     }
 }
